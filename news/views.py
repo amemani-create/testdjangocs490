6cab42ec6db0ce3b494d8418a34a0440e36e6bce
amemani-create <78717013+amemani-create@users.noreply.github.com>
2021-03-24 20:53:30 -0400

    no link version for news

--- news/views.py
+++ news/views.py
@@ -14,21 +14,18 @@ from newsapi import NewsApiClient
 
 def newsPaper(request):
     newsapi = NewsApiClient(api_key='573dab4634604cb0a5bc4a55de0f9e50')
-    top = newsapi.get_top_headlines(sources='new-scientist')
+    top = newsapi.get_top_headlines( sources='new-scientist')
 
     articles = top['articles']
-    description = []
-    newsTitle = []
+    desc = []
+    news = []
     img = []
-    url = []
 
     for i in range(len(articles)):
         newsInfo = articles[i]
-        newsTitle.append(newsInfo['title'])
-        description.append(newsInfo['description'])
+        news.append(newsInfo['title'])
+        desc.append(newsInfo['description'])
         img.append(newsInfo['urlToImage'])
-        url.append(newsInfo['url'])
+    Newslist = zip(news, desc, img)
 
-    Newslist = zip(newsTitle, description, url,img)
-
-    return render(request, 'index.html', context={"Newslist": Newslist})
+    return render(request, 'news.html', context={"Newslist": Newslist})
